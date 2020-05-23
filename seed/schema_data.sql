dec8750fbdc9df699d04a33ebb0f3dcc508b422b
koneru123 <koneru.priyanka@gmail.com>
2020-05-23 10:31:56 -0700

    address review comments

--- seed/schema_data.sql
+++ seed/schema_data.sql
@@ -4,22 +4,18 @@ CREATE DATABASE adidas;
 
 USE adidas;
 
-DROP TABLE IF EXISTS categoryInfo;
 CREATE TABLE categoryInfo(
-    categoryId INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    categoryId INT AUTO_INCREMENT PRIMARY KEY,
     categoryName VARCHAR(255)
 ) ENGINE=INNODB;
 
-DROP TABLE IF EXISTS productInfo;
 CREATE TABLE productInfo(
-    productId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    productId INT AUTO_INCREMENT PRIMARY KEY,
     productName VARCHAR(255),
     productPrice INT,
     dateCreated DATE,
     dateModified DATE,
     productImageURL VARCHAR(255),
     category INT
-    /* CONSTRAINT fk_categoryInfo_category FOREIGN KEY (category) 
-    REFERENCES categoryInfo(categoryId) */
 ) ENGINE=INNODB;
 
